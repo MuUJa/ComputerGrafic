353c5afa02bdbb465f1f1814115e9f74e688ba41
krllv <krllv@pm.me>
2023-03-06 23:29:57 +0300

    GlassEffect

--- GlassEffect.cs
+++ GlassEffect.cs
@@ -12,7 +12,28 @@ namespace ComputerGrafic
     {
         protected override Color CalculateNewPixelColor(Bitmap sourceImage, int x, int y)
         {
-            throw new NotImplementedException();
+            Color color = sourceImage.GetPixel(x, y);
+            Random rand = new Random();
+            int idX = (int)(x + (rand.NextDouble() - 0.5) * 10);
+            int idY = (int)(y + (rand.NextDouble() - 0.5) * 10);
+            if (Check(idX, 0, sourceImage.Width - 1) && Check(idY, 0, sourceImage.Height - 1))
+            {
+                return Color.FromArgb(sourceImage.GetPixel(idX, idY).A,
+                    sourceImage.GetPixel(idX, idY).R,
+                    sourceImage.GetPixel(idX, idY).G,
+                    sourceImage.GetPixel(idX, idY).B);
+            }
+            else
+                return Color.FromArgb(0, 0, 0, 0);
+
+        }
+        private bool Check(int value, int min, int max)
+        {
+            if (value < min)
+                return false;
+            if (value > max)
+                return false;
+            return true;
         }
     }
 }
